2e92adfd0350d3b0e5f596d6138cfbcdd5451522
condeac <condeadama9952@gmail.com>
2023-06-15 00:15:10 +0100

    calcommit

---
+++ calculatrice/Calculatrice/Calculatrice/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Calculatrice.Metier;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatrice
+{
+    public partial class Form1 : Form
+    {
+        Expression exp = new Expression();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Chiffre_Click(object sender, 
+            EventArgs e)
+        {
+            ecran.Text += (sender as Button).Text;
+
+        }
+        private void Operation_Click(object sender,
+    EventArgs e)
+        {
+            exp.A = ecran.Text;
+            exp.Op = (sender as Button).Text;
+            Effacer();
+        }
+
+        private void Effacer()
+        {
+            ecran.Text = "";
+        }
+
+        private void effacer_Click(object sender, EventArgs e)
+        {
+            Effacer();
+        }
+
+        private void egal_Click(object sender, EventArgs e)
+        {
+            exp.B = ecran.Text;
+            ecran.Text = exp.Evaluer();
+        }
+    }
+}
---
+++ calculatrice/Calculatrice/Calculatrice/Metier/Expression.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice.Metier
+{
+    internal class Expression
+    {
+        public string A { get; set; }   
+        public string B { get; set; }
+        public string Op { get; set; }
+        public Expression() { } 
+        public string Evaluer()
+        {
+
+            try
+            {
+                double valA = Double.Parse(A);
+                double valB = Double.Parse(B);
+                double c;
+
+                switch (Op)
+                {
+                    case "+": c = valA + valB; break;
+                    case "-": c = valA - valB; break;
+                    case "X": c = valA * valB; break;
+                    case "/": c = valA / valB; break;
+                    default: c = 0; break;
+                }
+                A = c.ToString();
+
+                return c.ToString();
+            }
+            catch (Exception)
+            {
+                return "Error";
+            }
+
+        }
+
+    }
+}
